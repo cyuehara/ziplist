a973382270a80a9a2f75aff01e9ff7bb7df8d5c0
Christine Uehara <42711372+cyuehara@users.noreply.github.com>
2020-02-10 22:12:41 -1000

    2nd attempt

--- ziplist.js
+++ ziplist.js
@@ -3,7 +3,7 @@ const testList2 = [1, 2, 3];
 
 function zipList(list1, list2){
   const resultList = [];
-  for (let i = 0; i < list1.length; i++){
+  for(let i = 0; i < list1.length; i++){
     resultList.push(list1[i], list2[i]);
   }
   return resultList;
